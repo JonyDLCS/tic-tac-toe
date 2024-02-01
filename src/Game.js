ccf5972538d31e8aac397ce178b9ad1fecbd1d60
Jonathan De La Cruz <jony_ed@hotmail.com>
2024-02-01 09:49:10 -0700

    first commit

---
+++ src/Game.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Board from './Board'
+
+
+function Game() {
+  return (
+    <div className='game' >
+        Tic-Tac-Toe
+        <Board/>
+    </div>
+  )
+}
+
+export default Game
